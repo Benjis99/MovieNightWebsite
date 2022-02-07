b7c5fe6054bfcc73cd7438af730f97c51bcb7c27
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2022-02-07 14:46:05 +0100

    added login function

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-function firstName() {
-  prompt("Firstname");
-}
-
-function lastName() {
-  prompt("Lastname");
-}
-
-function mailAdress() {
-  prompt();
-}
-
 function onClickMenu() {
   document.getElementById("menu").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+let firstName;
+let lastName;
+let email;
+
+function checkLoginMethod() {
+  firstName = document.getElementById("first").value;
+  lastName = document.getElementById("second").value;
+  email = document.getElementById("mail").value;
+  if (!isNaN(firstName)) {
+    alert("Bruh try again");
+  } else if (!isNaN(lastName)) {
+    alert("Bruh try again");
+  } else if (isNaN(firstName) || isNaN(lastName)) {
+    createAccount();
+  }
+
+  console.log(firstName, lastName, email);
+}
+
+function createAccount() {
+  alert(
+    `Your account was successfully created: ${firstName} ${lastName} ${email}`
+  );
 }
